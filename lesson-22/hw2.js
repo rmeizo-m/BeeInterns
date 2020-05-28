50caf1ea7c8e833cd222885c1a37f18470aaa5b5
rmeizo <design@elenabaranova.com>
2020-05-28 06:48:46 +0300

    lesson-22-update

---
+++ lesson-22/hw2.js
@@ -0,0 +1,16 @@
+// Задание 2
+/*
+ Написать функцию-конструктор Men с ДВУМЯ аргументами
+  С помощью которой можно создать объект
+  { profession: 'hero', name: 'Кларк', secondName: 'Кент' }
+*/
+function Men(name, secondName){
+    this.profession = 'hero';
+    this.name = name;
+    this.secondName:secondName;
+}
+
+
+const superman = new Men('Кларк', 'Кент');
+console.log('superman: ', superman);
+// Men { profession: 'hero', name: 'Кларк', secondName: 'Кент' }
