7b04d45c444abac1e0fb28db749704faed04accf
rmeizo <design@elenabaranova.com>
2020-06-27 03:46:27 +0300

    update-2

--- beeinterns-chat/script/script.js
+++ beeinterns-chat/script/script.js
@@ -1,40 +1,51 @@
-const output = document.querySelector(".chat");
-const inputBox = document.querySelector(".btn");
-const input = document.querySelector(".message__input");
+document.addEventListener("DOMContentLoaded", () => {
 
-input.addEventListener("input", () => {
-  if (input.value !== ""){
-    inputBox.style.background = `url("./images/path1.svg") no-repeat`;
-    inputBox.removeAttribute("disabled");
-  } else {
-    inputBox.style.background = `url("./images/path.svg") no-repeat`;
-    inputBox.setAttribute("disabled", "true");
-  }
-});
+  const output = document.querySelector(".chat");
+  const inputBox = document.querySelector(".btn");
+  const input = document.querySelector(".message__input");
+  var carent = ['Введи команду /start, для начала общения'];
 
-let carent = ['Привет, меня зовут Чат-бот, а как зовут тебя?'];
-
-  inputBox.addEventListener("click", () =>{
-    const inputText = input.value;
-    if(inputText === "/start"){
-        addCar = carent.push('Привет, меня зовут Чат-бот, а как зовут тебя?');
-    } else if(inputText.startsWith("/name:")){
-      carent.push('Привет, (name), приятно познакомится.');
-    } else if (inputText.startsWith("/number:")) {
-      alert('snumber');
-    } else if (inputText === "/stop") {
-      carent.push('Всего доброго, если хочешь поговорить пиши /start');
-    } else if (inputText === "/weather") {
-
-    } else{
-      alert('else');
+  input.addEventListener("input", () => {
+    if (input.value !== ""){
+      inputBox.style.background = `url("./images/path1.svg") no-repeat`;
+      inputBox.removeAttribute("disabled");
+    } else {
+      inputBox.style.background = `url("./images/path.svg") no-repeat`;
+      inputBox.setAttribute("disabled", "true");
     }
   });
 
-    function addDiv(){
-      for (const item of carent) {
-        addMessage(item);
+
+  inputBox.addEventListener("click", addHandler);
+
+  function addHandler(e){
+    e.preventDefault();
+    var inputText = input.value;
+    console.log(inputText);
+    if (inputText.length > 0) {
+      if(inputText == "/start"){
+        carent.push('Привет, меня зовут Чат-бот, а как зовут тебя?');
+      } else if(inputText.startsWith("/name:")){
+        carent.push('Привет, (name), приятно познакомится.');
+      } else if (inputText.startsWith("/number:")) {
+        alert('snumber');
+      } else if (inputText == "/stop") {
+        carent.push('Всего доброго, если хочешь поговорить пиши /start');
+      } else if (inputText == "/weather") {
+
+      } else{
+        alert('else');
       }
+
+      console.log(carent);
+      addDiv();
+    }
+  }
+
+  function addDiv(){
+    for (const item of carent) {
+      addMessage(item);
+    }
   }
 
   function addMessage(el){
@@ -43,6 +54,4 @@ let carent = ['Привет, меня зовут Чат-бот, а как зов
     elDiv.innerHTML = el;
     output.appendChild(elDiv);
   }
-
-// addMessage();
-addDiv();
+});
